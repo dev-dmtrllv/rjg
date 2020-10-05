4f1faf4338b6dba291b5d80baf1c0e0f6c7c0ecc
dev.dmtrllv <dev.dmtrllv@gmail.com>
2020-10-05 13:12:53 +0200

    added section title component

---
+++ components/SectionTitle.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { getClassFromProps } from "utils/react";
+
+import "./styles/section-title.scss";
+
+export const SectionTitle: React.FC<SectionTitleProps> = ({ title, subTitle, position = "center" }) => 
+{
+	return (
+		<div className={getClassFromProps("section-title", { [position]: true })}>
+			{subTitle && <h5 className="sub">{subTitle}</h5>}
+			<h2 className="title">{title}</h2>
+			<div className="underline" />
+		</div>
+	);
+};
+
+export type SectionTitleProps = {
+	position?: "left" | "center" | "right";
+	title: string;
+	subTitle: string;
+};
